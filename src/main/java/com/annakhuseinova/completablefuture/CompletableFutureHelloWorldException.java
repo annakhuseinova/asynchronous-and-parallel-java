056c2673a532ea87f6aae7760788489bda06e4aa
annakhuseinova <annakhuseinova@yandex.ru>
2020-11-24 21:24:26 +0300

    More examples

---
+++ src/main/java/com/annakhuseinova/completablefuture/CompletableFutureHelloWorldException.java
@@ -0,0 +1,41 @@
+package com.annakhuseinova.completablefuture;
+
+import com.annakhuseinova.service.HelloWorldService;
+
+import java.util.concurrent.CompletableFuture;
+
+import static com.annakhuseinova.util.CommonUtil.*;
+
+public class CompletableFutureHelloWorldException {
+
+    private HelloWorldService helloWorldService;
+
+    public CompletableFutureHelloWorldException(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    public String helloWorld_3_async_calls_handle_exception() {
+        startTimer();
+
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(() -> helloWorldService.hello());
+        CompletableFuture<String> world = CompletableFuture.supplyAsync(() -> helloWorldService.world());
+        CompletableFuture<String> hiCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            delay(1000);
+            return "Hi, CompletableFuture!";
+        });
+        String helloWorld = hello.handle((result, exception)-> {
+            System.out.println("Exception is :" + exception.getMessage());
+            return "";
+        })
+                .thenCombine(world, (h, w) -> h + w)
+                .handle((result, exception)-> {
+                    System.out.println(exception.getMessage());
+                    return "";
+                })
+                .thenCombine(hiCompletableFuture, (previous, current) -> previous + current)
+                .thenApply(String::toUpperCase)
+                .join();
+        timeTaken();
+        return helloWorld;
+    }
+}
---
+++ src/test/java/com/annakhuseinova/completablefuture/CompletableFutureHelloWorldExceptionTest.java
@@ -0,0 +1,42 @@
+package com.annakhuseinova.completablefuture;
+
+import com.annakhuseinova.service.HelloWorldService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
+class CompletableFutureHelloWorldExceptionTest {
+
+    @Mock
+    private HelloWorldService helloWorldService;
+
+    @InjectMocks
+    private CompletableFutureHelloWorldException completableFutureHelloWorldException;
+
+    @Test
+    void helloWorld_3_async_calls() {
+        when(helloWorldService.hello()).thenThrow(new RuntimeException("Exception occurred"));
+        when(helloWorldService.world()).thenCallRealMethod();
+
+        String result = completableFutureHelloWorldException.helloWorld_3_async_calls_handle_exception();
+
+        assertEquals(" WORLD! HI COMPLETABLE FUTURE!", result);
+    }
+
+    @Test
+    void helloWorld_3_async_calls_2() {
+        when(helloWorldService.hello()).thenThrow(new RuntimeException("Exception occurred"));
+        when(helloWorldService.world()).thenCallRealMethod();
+
+        String result = completableFutureHelloWorldException.helloWorld_3_async_calls_handle_exception();
+
+        assertEquals(" WORLD! HI COMPLETABLE FUTURE!", result);
+    }
+}
